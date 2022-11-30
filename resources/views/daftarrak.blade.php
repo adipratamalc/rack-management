e87e0477b0575697475ea28ea0b43d4384a0e171
cahyadikrishna <cahyadikrishna.ck@gmail.com>
2022-11-30 15:03:29 +0800

    Implement daftar rak page

---
+++ resources/views/daftarrak.blade.php
@@ -0,0 +1,201 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="main-content">
+  <section class="section">
+    <div class="section-header">
+      <h1>Daftar Rak</h1>
+      <div class="section-header-breadcrumb">
+        <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+        <div class="breadcrumb-item"><a href="#">Layout</a></div>
+        <div class="breadcrumb-item">Daftar Rak</div>
+      </div>
+    </div>
+
+    <div class="section-body">
+      <div class="row">
+        <div class="col-12 col-md-6 col-lg-3">
+          <div class="card py-3">
+            <div class="px-3">
+              <h6 class="h5 font-weight-bold text-primary">RAK A</h6>
+            </div>
+
+            <div class="px-3">
+              <div class="main-row">
+                <p class="h5 mb-1">First</p>
+                <div class="p-2 bg-light text-center border border-danger">
+                  <div class="bg-secondary mb-2 p-1">Top</div>
+                  <div class="bg-danger mb-2 p-1 text-white">Middle</div>
+                  <div class="bg-secondary p-1">Bottom</div>
+                </div>
+              </div>
+
+              <div class="main-row mt-3">
+                <p class="h5 mb-1">Second</p>
+                <div class="p-2 bg-light text-center">
+                  <div class="bg-secondary mb-2 p-1">Top</div>
+                  <div class="bg-secondary mb-2 p-1">Middle</div>
+                  <div class="bg-secondary p-1">Bottom</div>
+                </div>
+              </div>
+
+              <div class="main-row mt-3">
+                <p class="h5 mb-1">Third</p>
+                <div class="p-2 bg-light text-center">
+                  <div class="bg-secondary mb-2 p-1">Top</div>
+                  <div class="bg-secondary mb-2 p-1">Middle</div>
+                  <div class="bg-secondary p-1">Bottom</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-3">
+          <div class="card py-3">
+            <div class="px-3">
+              <h6 class="h5 font-weight-bold text-primary">RAK A</h6>
+            </div>
+
+            <div class="px-3">
+              <div class="main-row">
+                <p class="h5 mb-1">First</p>
+                <div class="p-2 bg-light text-center">
+                  <div class="bg-secondary mb-2 p-1">Top</div>
+                  <div class="bg-secondary mb-2 p-1">Middle</div>
+                  <div class="bg-secondary p-1">Bottom</div>
+                </div>
+              </div>
+
+              <div class="main-row mt-3">
+                <p class="h5 mb-1">Second</p>
+                <div class="p-2 bg-light text-center">
+                  <div class="bg-secondary mb-2 p-1">Top</div>
+                  <div class="bg-secondary mb-2 p-1">Middle</div>
+                  <div class="bg-secondary p-1">Bottom</div>
+                </div>
+              </div>
+
+              <div class="main-row mt-3">
+                <p class="h5 mb-1">Third</p>
+                <div class="p-2 bg-light text-center">
+                  <div class="bg-secondary mb-2 p-1">Top</div>
+                  <div class="bg-secondary mb-2 p-1">Middle</div>
+                  <div class="bg-secondary p-1">Bottom</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-3">
+          <div class="card py-3">
+            <div class="px-3">
+              <h6 class="h5 font-weight-bold text-primary">RAK A</h6>
+            </div>
+
+            <div class="px-3">
+              <div class="main-row">
+                <p class="h5 mb-1">First</p>
+                <div class="p-2 bg-light text-center">
+                  <div class="bg-secondary mb-2 p-1">Top</div>
+                  <div class="bg-secondary mb-2 p-1">Middle</div>
+                  <div class="bg-secondary p-1">Bottom</div>
+                </div>
+              </div>
+
+              <div class="main-row mt-3">
+                <p class="h5 mb-1">Second</p>
+                <div class="p-2 bg-light text-center">
+                  <div class="bg-secondary mb-2 p-1">Top</div>
+                  <div class="bg-secondary mb-2 p-1">Middle</div>
+                  <div class="bg-secondary p-1">Bottom</div>
+                </div>
+              </div>
+
+              <div class="main-row mt-3">
+                <p class="h5 mb-1">Third</p>
+                <div class="p-2 bg-light text-center">
+                  <div class="bg-secondary mb-2 p-1">Top</div>
+                  <div class="bg-secondary mb-2 p-1">Middle</div>
+                  <div class="bg-secondary p-1">Bottom</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-3">
+          <div class="card py-3">
+            <div class="px-3">
+              <h6 class="h5 font-weight-bold text-primary">RAK A</h6>
+            </div>
+
+            <div class="px-3">
+              <div class="main-row">
+                <p class="h5 mb-1">First</p>
+                <div class="p-2 bg-light text-center">
+                  <div class="bg-secondary mb-2 p-1">Top</div>
+                  <div class="bg-secondary mb-2 p-1">Middle</div>
+                  <div class="bg-secondary p-1">Bottom</div>
+                </div>
+              </div>
+
+              <div class="main-row mt-3">
+                <p class="h5 mb-1">Second</p>
+                <div class="p-2 bg-light text-center">
+                  <div class="bg-secondary mb-2 p-1">Top</div>
+                  <div class="bg-secondary mb-2 p-1">Middle</div>
+                  <div class="bg-secondary p-1">Bottom</div>
+                </div>
+              </div>
+
+              <div class="main-row mt-3">
+                <p class="h5 mb-1">Third</p>
+                <div class="p-2 bg-light text-center">
+                  <div class="bg-secondary mb-2 p-1">Top</div>
+                  <div class="bg-secondary mb-2 p-1">Middle</div>
+                  <div class="bg-secondary p-1">Bottom</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-3">
+          <div class="card py-3">
+            <div class="px-3">
+              <h6 class="h5 font-weight-bold text-primary">RAK A</h6>
+            </div>
+
+            <div class="px-3">
+              <div class="main-row">
+                <p class="h5 mb-1">First</p>
+                <div class="p-2 bg-light text-center">
+                  <div class="bg-secondary mb-2 p-1">Top</div>
+                  <div class="bg-secondary mb-2 p-1">Middle</div>
+                  <div class="bg-secondary p-1">Bottom</div>
+                </div>
+              </div>
+
+              <div class="main-row mt-3">
+                <p class="h5 mb-1">Second</p>
+                <div class="p-2 bg-light text-center">
+                  <div class="bg-secondary mb-2 p-1">Top</div>
+                  <div class="bg-secondary mb-2 p-1">Middle</div>
+                  <div class="bg-secondary p-1">Bottom</div>
+                </div>
+              </div>
+
+              <div class="main-row mt-3">
+                <p class="h5 mb-1">Third</p>
+                <div class="p-2 bg-light text-center">
+                  <div class="bg-secondary mb-2 p-1">Top</div>
+                  <div class="bg-secondary mb-2 p-1">Middle</div>
+                  <div class="bg-secondary p-1">Bottom</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+@endsection
