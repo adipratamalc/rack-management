4adb79d5411d5ece3a46dc2ad250dc9b7325aa43
adipratamalc <adipratama.lc@gmail.com>
2022-12-04 22:21:16 +0800

    Add trait UUID

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -2,14 +2,16 @@
 
 namespace App\Models;
 
+use App\Traits\uuidAsKey;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Barang extends Model
 {
-  use HasFactory;
+  use HasFactory, uuidAsKey;
   public $fllable = ['kode_barang', 'nama_barang', 'stok', 'harga'];
   public $timestamps = true;
+
 
   public function jenis_barang()
   {
--- app/Models/Rak.php
+++ app/Models/Rak.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\uuidAsKey;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Rak extends Model
 {
-  use HasFactory;
+  use HasFactory, uuidAsKey;
   public $fllable = ['kode_rak'];
   public $timestamps = true;
 
